cc76564f25c1a6a3b01c719920d142104102dea2
Akansha Salwan <akankshasalwan@gmail.com>
2024-08-11 23:16:36 +0530

    header update

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,34 +1,34 @@
 import React from 'react';
 import '../header/header.css';
-import Logo from '../../assets/images/logo.svg';
+import Logo from '../../assets/images/logo.png';
+import SearchIcon from '@mui/icons-material/Search';
 
 
 const Header =() =>{
     return(
         <>
         <header>
-            <div className='container-fluid'>
-                <div className='row'>
-                    <div className='col-sm-2'>
-                      <img src={Logo} />
-                    </div>
-
-               {/* headersearch start */}
-
-               <div className='col-sm-5'>
-                  <div className='headerSearch d-flex align-item-center'>
-                      <div className='selectDrop cursor'>
-                        All Categories
+          <div className='container-fluid'>
+               <div className='row'>
+                      <div className='col-sm-2'>
+                           <img src={Logo} />
                       </div>
-                      <div className='search'>
-                        <input type="text" placeholder='Search for items.....'/>
+
+                         
+                     
+                      <div className='col-sm-5'>
+                          <div className='headerSearch d-flex align-item-center'>
+                               <div className='selectDrop cursor'>
+                                   All Categories
+                               </div>
+                               <div className='search'>
+                                 <input type="text" placeholder='Search for items.....'/>
+                                 <SearchIcon className='SearchIcon' />
+                               </div>
+                          </div>
                       </div>
-                  </div>
                </div>
-
-             {/* headersearch end */}
-                </div>
-           </div>
+          </div>
         </header>
         </>
     )
