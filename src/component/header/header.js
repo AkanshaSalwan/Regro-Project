e0ec0a7a901bb528e8e88c1a49d7c18f4fe156f1
Akansha Salwan <akankshasalwan@gmail.com>
2024-08-16 23:30:49 +0530

    header update

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../header/header.css';
 import Logo from '../../assets/images/logo.svg';
+import logo from '../../assets/images/logo1.svg'
 import SearchIcon from '@mui/icons-material/Search';
 import Select from '../selectDrop/select';
+import Compare from '../../assets/images/icon-compare.svg';
+import Heart from '../../assets/images/icon-heart.svg';
+import Cart from '../../assets/images/icon-cart.svg';
+import User from '../../assets/images/icon-user.svg';
+import Button from '@mui/material/Button';
+
+
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
 
 
 const Header = () => {
+
+  const [isOpenDropDown, setisOpenDropDown] = useState(false);
   return (
     <>
       <header>
         <div className='container-fluid'>
           <div className='row'>
             <div className='col-sm-2'>
-              <img src={Logo} />
+              <img src={logo} />
             </div>
 
             {/* header start */}
@@ -31,6 +47,61 @@ const Header = () => {
               </div>
             </div>
             {/* header end */}
+
+
+            {/*  */}
+            {/*  */}
+                 <div className='col-sm-5 d-flex align-items-center'>
+                   <div  className='list list-inline mb-0 headerTabs'>
+                      <div className='countryWrapper'>
+
+                      </div>
+
+                      <ul className='list list-inline mb-0 headerTab'>
+                        <li className='list-inline-item'>
+                          <span>
+                            <img src={Compare}/>
+                            <span className='badge bg-success rounded-circle'>3</span>
+                            Compare
+                            </span>
+                        </li>
+                        <li className='list-inline-item'>
+                          <span>
+                            <img src={Heart}/>
+                            <span className='badge bg-success rounded-circle'>8</span>
+                            Wishlist
+                            </span>
+                        </li>
+                        <li className='list-inline-item'>
+                          <span>
+                            <img src={Cart}/>
+                            <span className='badge bg-success rounded-circle'>2</span>
+                           Cart
+                            </span>
+                        </li>
+                        <li className='list-inline-item' >
+                            <span onClick={()=>setisOpenDropDown(!isOpenDropDown)}>
+                              <img src={User}/>
+                               Account
+                            </span>
+
+                            {/* drop down list */}
+                            { 
+                            isOpenDropDown!==false &&
+                            <ul className='dropdownMenu'>
+                              <li><Button> <Person2OutlinedIcon></Person2OutlinedIcon>My Account</Button></li>
+                              <li><Button> <LocationOnOutlinedIcon></LocationOnOutlinedIcon>Order Tracking</Button></li>
+                              <li><Button> <FavoriteBorderOutlinedIcon></FavoriteBorderOutlinedIcon>My Voucher</Button></li>
+                              <li><Button> <SettingsOutlinedIcon></SettingsOutlinedIcon>Setting</Button></li>
+                              <li><Button> <LockOutlinedIcon></LockOutlinedIcon>Sign out</Button></li>
+                            </ul>
+                            }
+                        </li>
+                      </ul>
+                   </div>
+                 </div>
+
+         
           </div>
         </div>
       </header>
