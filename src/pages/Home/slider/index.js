0e04ca63cdac41783e24081c017a21a90468da2d
Akansha Salwan <akankshasalwan@gmail.com>
2024-08-18 22:55:08 +0530

    header and hero section update

--- src/pages/Home/slider/index.js
+++ src/pages/Home/slider/index.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import Slider from "react-slick";
 import './index.css';
+import '../../../assets/images/image/slideBanner1.jpg';
+import Slide1 from '../../../assets/images/slider-1.png';
+import Slide2 from '../../../assets/images/slider-2.png';
+// import '../../../../node_modules/react-slick';
 
 
-const  HomeSlider =()=>{
+const HomeSlider =()=>{
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade:true, //slider slider automatic no need to tap  on slide button
+      //   arrows:true
+      };
+
+//       <i class="fa fa-angle-left" aria-hidden="true"></i>  issue in 24:26 #4
+// <i class="fa fa-angle-right" aria-hidden="true"></i>
     return(
         <> 
         <section className='homeSlider'>
-            <div className='container-fluid'>
-
-            </div>
+        {/* <i class="fa fa-angle-left" aria-hidden="true"></i> 
+        <i class="fa fa-angle-right" aria-hidden="true"></i> */}
+         <div className='container-fluid'>
+            <Slider {...settings} className='home_Slider_main'>
+               <div className='item'>
+                 <img src={Slide1} className='w-100'/>
+              </div>
+              <div className='item'>
+                 <img src={Slide2} className='w-100'/>
+              </div>
+            </Slider>
+         </div>
         </section>
+        <Slider></Slider>
         </>
-    )
+    )  
 }
 
 export default HomeSlider;
