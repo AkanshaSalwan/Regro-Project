430fb51f986bec8f35d0474017bbfaf168f44a07
Akansha Salwan <akankshasalwan@gmail.com>
2024-08-19 23:01:17 +0530

    hero section update

--- src/pages/Home/slider/index.js
+++ src/pages/Home/slider/index.js
@@ -5,6 +5,7 @@ import '../../../assets/images/image/slideBanner1.jpg';
 import Slide1 from '../../../assets/images/slider-1.png';
 import Slide2 from '../../../assets/images/slider-2.png';
 // import '../../../../node_modules/react-slick';
+import Button from '@mui/material/Button';
 
 
 const HomeSlider =()=>{
@@ -26,7 +27,7 @@ const HomeSlider =()=>{
         <section className='homeSlider'>
         {/* <i class="fa fa-angle-left" aria-hidden="true"></i> 
         <i class="fa fa-angle-right" aria-hidden="true"></i> */}
-         <div className='container-fluid'>
+         <div className='container-fluid position-relative'>
             <Slider {...settings} className='home_Slider_main'>
                <div className='item'>
                  <img src={Slide1} className='w-100'/>
@@ -39,8 +40,19 @@ const HomeSlider =()=>{
               </div>
               <div className='item'>
                  <img src={Slide2} className='w-100'/>
+                  <div className='info'>
+                   <h2 className='mb-4'>
+                    Fresh Vegetables<br></br>Big discount
+                   </h2>
+                   <p>Save up to 50% off on your First order</p>
+                 </div>
               </div>
             </Slider>
+
+            <div className='newsLetterBanner'>
+              <input type="text" placeholder='your email address..' />
+              <Button className='bg-g'>Subscribe</Button>
+            </div>
          </div>
         </section>
         <Slider></Slider>
