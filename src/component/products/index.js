71d1150e4b9cf7e23cd74ab56db3abe811245d1a
Akansha Salwan <akankshasalwan@gmail.com>
2024-08-22 16:40:19 +0530

    update Display products in home page

--- src/component/products/index.js
+++ src/component/products/index.js
@@ -3,31 +3,34 @@ import "./style.css";
 import Rating from "@mui/material/Rating";
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Product = () => {
   return (
     <>
       <div className="productThumb">
-        <div className="imgWrapper">
-          <img
+      <Link>
+         <div className="imgWrapper">
+            <img
             src="https://nest-frontend-v6.netlify.app/assets/imgs/shop/product-2-2.jpg"
             className="w-100"
-            alt=""
-          />
-        </div>
+            alt=""/>
+         </div>
+      </Link>
         <div className="info">
           <span className="d-block catName">Hodo Foods</span>
           <h4 className="title"><Link>All Natural Italian-Style Chicken Meatballs</Link></h4>
           <Rating name="read-only" value={4} readOnly />
-          <span className="brand d-block">
-            By <a className="text-g "> Stouffer </a>
+          <span className="brand d-block text-g">
+            By <Link className="text-g "> Stouffer </Link>
           </span>
 
-          <div className="d-flex align-items-center">
+          <div className="d-flex align-items-center mt-3">
             <div className="d-flex align-items-center">
-               <span className="price"> $17.85</span> <span className="oldPrice"> $19.8</span>
+               <span className="price text-g font-weight-bold"> $17.85</span> <span className="oldPrice"> $19.8</span>
             </div>
-            <Button className="bg-g ml-auto">
+            <Button className=" ml-auto transition">
+              <ShoppingCartOutlinedIcon></ShoppingCartOutlinedIcon>
                Add
             </Button>
           </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,79 +1,82 @@
-import React from 'react';
-import Slider from './slider/index';
-import CatSlider from '../../component/catSlider';
+import React from "react";
+import Slider from "./slider/index";
+import CatSlider from "../../component/catSlider";
 
-import Banners from '../../component/banners';
-import './index.css';
-import  Products from '../../component/products';
-
-
-
+import Banners from "../../component/banners";
+import "./index.css";
+import Products from "../../component/products";
 
 const Home = () => {
   return (
     <>
-    <Slider ></Slider>
-    <CatSlider></CatSlider>
-    <Banners></Banners>
+      <Slider></Slider>
+      <CatSlider></CatSlider>
+      <Banners></Banners>
 
-
-    <section className='homeProducts'>
-      <div className='container-fluid'>
-        <div className='d-flex align-item-center'>
-        <h2 className='hd mb-0 mt-0'>Popular Products</h2>
-        <ul className='list list-inline ml-auto filterTab'>
-            <li className='list-inline-item'>
-              <a href="cursor">All</a>
-            </li>
-            <li className='list-inline-item'>
-              <a href="cursor">Milks & Dairies</a>
-            </li>
-            <li className='list-inline-item'>
-              <a href="cursor">Coffes & Teas</a>
-            </li>
-            <li className='list-inline-item'>
-              <a href="cursor">Pet Foods</a>
-            </li>
-            <li className='list-inline-item'>
-              <a href="cursor">Meats</a>
-            </li>
-            <li className='list-inline-item'>
-              <a href="cursor">Vegetables</a>
-            </li>
-            <li className='list-inline-item'>
-              <a href="cursor">Fruits</a>
-            </li>
-        </ul>
+      <section className="homeProducts">
+        <div className="container-fluid">
+          <div className="d-flex align-item-center">
+            <h2 className="hd mb-0 mt-0">Popular Products</h2>
+            <ul className="list list-inline ml-auto filterTab">
+              <li className="list-inline-item">
+                <a href="cursor">All</a>
+              </li>
+              <li className="list-inline-item">
+                <a href="cursor">Milks & Dairies</a>
+              </li>
+              <li className="list-inline-item">
+                <a href="cursor">Coffes & Teas</a>
+              </li>
+              <li className="list-inline-item">
+                <a href="cursor">Pet Foods</a>
+              </li>
+              <li className="list-inline-item">
+                <a href="cursor">Meats</a>
+              </li>
+              <li className="list-inline-item">
+                <a href="cursor">Vegetables</a>
+              </li>
+              <li className="list-inline-item">
+                <a href="cursor">Fruits</a>
+              </li>
+            </ul>
+          </div>
+          <div className="row productRow">
+            <div className="item">
+              <Products></Products>
+            </div>
+            <div className="item">
+              <Products></Products>
+            </div>
+            <div className="item">
+              <Products></Products>
+            </div>
+            <div className="item">
+              <Products></Products>
+            </div>
+            <div className="item">
+              <Products></Products>
+            </div>
+            <div className="item">
+              <Products></Products>
+            </div>
+            <div className="item">
+              <Products></Products>
+            </div>
+            <div className="item">
+              <Products></Products>
+            </div>
+            <div className="item">
+              <Products></Products>
+            </div>
+            <div className="item">
+              <Products></Products>
+            </div>
+          </div>
         </div>
-
-
-        <div className='row productRow'>
-          <div className='item'>
-            <Products></Products>
-          </div>
-          <div className='item'>
-            <Products></Products>
-          </div>
-          <div className='item'>
-            <Products></Products>
-          </div>
-          <div className='item'>
-            <Products></Products>
-          </div>
-          <div className='item'>
-            <Products></Products>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Home; 
-
-
-
-
-
-
+export default Home;
