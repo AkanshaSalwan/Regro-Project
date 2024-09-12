207cf88ab6dfae69859f5f37fef0737ce3ce166a
Akansha Salwan <akankshasalwan@gmail.com>
2024-09-12 12:44:52 +0530

    updates in shopping cart

--- src/component/quantityBox/quantity.js
+++ src/component/quantityBox/quantity.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import { useState } from "react";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 
 const QuantityBox = () => {
+  const [inputValue, setinputValue] = useState(1);
+
+  const plus = () => {
+    setinputValue(inputValue + 1);
+  };
+  const minus = () => {
+    if (inputValue !== 1) {
+      setinputValue(inputValue - 1);
+    }
+  };
+
   return (
     <>
-    
+      <div className="addCartSection pt-4 pb-4 d-flex align-items-center">
+        <div className="counterSec mr-3">
+          <input type="number" value={inputValue} />
+          <span className="arrow plus" onClick={plus}>
+            <IoIosArrowUp></IoIosArrowUp>
+          </span>
+
+          <span className=" arrow minus" onClick={minus}>
+            <IoIosArrowDown></IoIosArrowDown>
+          </span>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default QuantityBox;
--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,25 +1,16 @@
-import  {useState } from "react";
+import React from 'react';
 import "./cart.css";
 import { Link } from "react-router-dom";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import { Button } from "@mui/material";
 import Rating from "@mui/material/Rating";
-import { IoIosArrowUp } from "react-icons/io";
-import { IoIosArrowDown } from "react-icons/io";
-import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
-import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+
+import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
+import QuantityBox from "../../component/quantityBox/quantity";
 
 const Cart = () => {
-  const [inputValue, setinputValue] = useState(1);
-
-  const plus = () => {
-    setinputValue(inputValue + 1);
-  };
-  const minus = () => {
-    if (inputValue !== 1) {
-      setinputValue(inputValue - 1);
-    }
-  };
+ 
   return (
     <>
       <div className="breadcrumbWrapper mb-4">
@@ -85,32 +76,25 @@ const Cart = () => {
                                 </h4>
                               </Link>
                               <Rating name="read-only" value={4.0} readOnly />{" "}
-                              <span className="text-light">(4.0)</span>
+                              <h5 className="text-light">(4.0)</h5>
                             </div>
                           </div>
                         </td>
 
                         <td className="text-gray">$2.51</td>
                         <td>
-                        
-                          <div className="addCartSection pt-4 pb-4 d-flex align-items-center">
-                            <div className="counterSec mr-3">
-                              <input type="number" value={inputValue} />
-                              <span className="arrow plus" onClick={plus}>
-                                <IoIosArrowUp></IoIosArrowUp>
-                              </span>
-
-                              <span className=" arrow minus" onClick={minus}>
-                                <IoIosArrowDown></IoIosArrowDown>
-                              </span>
-                            </div>
-                          </div>
+                          < QuantityBox></QuantityBox>
                         </td>
-                       
-                       <td><span className="text-g">$2.56</span></td>
 
-                       <td><span className="cursor"><DeleteForeverOutlinedIcon></DeleteForeverOutlinedIcon></span></td>
-                         
+                        <td>
+                          <span className="text-g">$2.56</span>
+                        </td>
+
+                        <td>
+                          <span className="cursor">
+                            <DeleteForeverOutlinedIcon></DeleteForeverOutlinedIcon>
+                          </span>
+                        </td>
                       </tr>
 
                       <tr>
@@ -130,32 +114,25 @@ const Cart = () => {
                                 </h4>
                               </Link>
                               <Rating name="read-only" value={4.0} readOnly />{" "}
-                              <span className="text-light">(4.0)</span>
+                              <h5 className="text-light">(4.0)</h5>
                             </div>
                           </div>
                         </td>
 
                         <td className="text-gray">$2.51</td>
                         <td>
-                        
-                          <div className="addCartSection pt-4 pb-4 d-flex align-items-center">
-                            <div className="counterSec mr-3">
-                              <input type="number" value={inputValue} />
-                              <span className="arrow plus" onClick={plus}>
-                                <IoIosArrowUp></IoIosArrowUp>
-                              </span>
-
-                              <span className=" arrow minus" onClick={minus}>
-                                <IoIosArrowDown></IoIosArrowDown>
-                              </span>
-                            </div>
-                          </div>
+                        < QuantityBox></QuantityBox>
                         </td>
-                       
-                       <td><span className="text-g">$2.56</span></td>
 
-                       <td><span className="cursor"><DeleteForeverOutlinedIcon></DeleteForeverOutlinedIcon></span></td>
-                         
+                        <td>
+                          <span className="text-g">$2.56</span>
+                        </td>
+
+                        <td>
+                          <span className="cursor">
+                            <DeleteForeverOutlinedIcon></DeleteForeverOutlinedIcon>
+                          </span>
+                        </td>
                       </tr>
 
                       <tr>
@@ -175,32 +152,25 @@ const Cart = () => {
                                 </h4>
                               </Link>
                               <Rating name="read-only" value={4.0} readOnly />{" "}
-                              <span className="text-light">(4.0)</span>
+                              <h5 className="text-light">(4.0)</h5>
                             </div>
                           </div>
                         </td>
 
                         <td className="text-gray">$2.51</td>
                         <td>
-                        
-                          <div className="addCartSection pt-4 pb-4 d-flex align-items-center">
-                            <div className="counterSec mr-3">
-                              <input type="number" value={inputValue} />
-                              <span className="arrow plus" onClick={plus}>
-                                <IoIosArrowUp></IoIosArrowUp>
-                              </span>
-
-                              <span className=" arrow minus" onClick={minus}>
-                                <IoIosArrowDown></IoIosArrowDown>
-                              </span>
-                            </div>
-                          </div>
+                        < QuantityBox></QuantityBox>
                         </td>
-                       
-                       <td><span className="text-g">$2.56</span></td>
 
-                       <td><span className="cursor"><DeleteForeverOutlinedIcon></DeleteForeverOutlinedIcon></span></td>
-                         
+                        <td>
+                          <span className="text-g">$2.56</span>
+                        </td>
+
+                        <td>
+                          <span className="cursor">
+                            <DeleteForeverOutlinedIcon></DeleteForeverOutlinedIcon>
+                          </span>
+                        </td>
                       </tr>
                     </tbody>
                   </table>
@@ -209,30 +179,42 @@ const Cart = () => {
             </div>
 
             <div className="col-md-5 pl-5  cartRightBox ">
-               <div className="card p-4 ">
+              <div className="card p-4 ">
                 <div className="d-flex align-items-center mb-4">
                   <h5 className="mb-0 text-gray">Subtotal</h5>
-                  <h3 className="ml-auto mb-0 font-weight-bold"><span className="text-g">$12.89</span></h3>
+                  <h3 className="ml-auto mb-0 font-weight-bold">
+                    <span className="text-g">$12.89</span>
+                  </h3>
                 </div>
-                       <hr />
+                <hr />
                 <div className="d-flex align-items-center mb-4">
                   <h5 className="mb-0 text-gray">Shipping</h5>
-                  <h3 className="ml-auto mb-0 "><span className="text-blue">Free</span></h3>
+                  <h3 className="ml-auto mb-0 ">
+                    <span className="text-blue">Free</span>
+                  </h3>
                 </div>
                 <hr />
                 <div className="d-flex align-items-center mb-4">
                   <h5 className="mb-0 text-gray">Estimate for</h5>
-                  <h3 className="ml-auto mb-0 "><span className="text-blue">United Kingdom</span></h3>
+                  <h3 className="ml-auto mb-0 ">
+                    <span className="text-blue">United Kingdom</span>
+                  </h3>
                 </div>
                 <hr />
                 <div className="d-flex align-items-center mb-4">
                   <h5 className="mb-0 text-gray">Total</h5>
-                  <h3 className="ml-auto mb-0 font-weight-bold"><span className="text-g">$12.89</span></h3>
+                  <h3 className="ml-auto mb-0 font-weight-bold">
+                    <span className="text-g">$12.89</span>
+                  </h3>
                 </div>
                 <hr />
-                    <Button className="btn-g btn-lg"><span>< ExitToAppOutlinedIcon></ExitToAppOutlinedIcon></span> Proceed To CheckOut</Button>
-              
-               </div>
+                <Button className="btn-g btn-lg">
+                  <span>
+                    <ExitToAppOutlinedIcon></ExitToAppOutlinedIcon>
+                  </span>{" "}
+                  Proceed To CheckOut
+                </Button>
+              </div>
             </div>
           </div>
         </div>
